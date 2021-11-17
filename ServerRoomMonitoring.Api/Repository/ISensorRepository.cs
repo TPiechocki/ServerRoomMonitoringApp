29f354741cce6b5723de2fbb00d98e613c08660f
Myszon <szmnnagel@gmail.com>
2021-11-17 12:31:49 +0100

    Added API Project

---
+++ ServerRoomMonitoring.Api/Repository/ISensorRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using ServerRoomMonitoring.Api.Models;
+
+namespace ServerRoomMonitoring.Api.Repository
+{
+    public interface ISensorRepository
+    {
+
+        public List<SensorMessage> GetAllSensors();
+
+        public void AddSensor(SensorMessage sensorMessage);
+        
+        public List<SensorMessage> GetByTypeSensors(string type);
+
+
+    }
+
+
+
+}
---
+++ ServerRoomMonitoring.Api/Services/SensorService.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Collections.Generic;
+using ServerRoomMonitoring.Api.Models;
+using ServerRoomMonitoring.Api.Repository;
+
+namespace ServerRoomMonitoring.Api.Services
+{
+    public class SensorService: ISensorService
+    {
+        private ISensorRepository _sensorRepository;
+
+        public SensorService(ISensorRepository sensorRepository)
+        {
+            _sensorRepository = sensorRepository;
+        }
+
+        public List<SensorMessage> GetAllSensors()
+        {
+            return _sensorRepository.GetAllSensors();
+        }
+
+        public void AddSensor(SensorMessage sensorMessage)
+        {
+           _sensorRepository.AddSensor(sensorMessage);
+        }
+
+        public List<SensorMessage> GetByTypeSensors(string type)
+        {
+            return _sensorRepository.GetByTypeSensors(type);
+        }
+    }
+
+
+
+}
