a08fc7601204cd356e99339cd24b4aa9dc02687c
theATM <the_atm@wp.pl>
2021-12-06 22:44:58 +0100

    Hello World Controller

---
+++ DockerComposeDev.bat
@@ -0,0 +1,1 @@
+docker compose -f .\docker-compose.developer.yml -p serverroom up -d
---
+++ ServerRoomMonitoring.Api/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerRoomMonitoring.Api.Controllers
+{
+    [Route("[controller]")]
+    public class ApiController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Ok("This is api");
+        }
+    }
+}
