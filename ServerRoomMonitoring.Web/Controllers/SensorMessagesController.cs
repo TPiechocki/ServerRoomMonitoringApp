4d11d198e027e61e9f6ac844a5d2700e4da81fdf
Magdalena-N <magdusiamn@gmail.com>
2021-12-06 21:20:39 +0100

    Add Id sorting

--- ServerRoomMonitoring.Web/Controllers/SensorMessagesController.cs
+++ ServerRoomMonitoring.Web/Controllers/SensorMessagesController.cs
@@ -25,6 +25,7 @@ namespace ServerRoomMonitoring.Web.Controllers
         // GET: SensorMessages
         public IActionResult Index(string sortOrder)
         {
+            ViewData["IdSortParm"] = sortOrder == "Id" ? "id_desc" : "Id";
             ViewData["TypeSortParm"] = sortOrder == "Type" ? "type_desc" : "Type";
             ViewData["ValueSortParm"] = sortOrder == "Value" ? "value_desc" : "Value";
             ViewData["UnitSortParm"] = sortOrder == "Unit" ? "unit_desc" : "Unit";
@@ -33,6 +34,12 @@ namespace ServerRoomMonitoring.Web.Controllers
                            select s;
             switch (sortOrder)
             {
+                case "Id":
+                    sensors = sensors.OrderBy(s => s.Id);
+                    break;
+                case "id_desc":
+                    sensors = sensors.OrderByDescending(s => s.Id);
+                    break;
                 case "Type":
                     sensors = sensors.OrderBy(s => s.SensorType);
                     break;
